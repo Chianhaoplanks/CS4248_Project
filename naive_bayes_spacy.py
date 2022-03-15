48b4de913414e3c9fa4cff9d2fd2860cb6efb968
rabhijit <abhijit.ravichandran@gmail.com>
2022-03-15 20:23:21 +0800

    working decently with LR

--- naive_bayes_spacy.py
+++ naive_bayes_spacy.py
@@ -2,9 +2,8 @@ import string
 import re
 
 import pandas as pd
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
-from nltk.stem.porter import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
 
 from sklearn.metrics import f1_score, recall_score, precision_score
 from sklearn.pipeline import Pipeline, FeatureUnion
@@ -12,23 +11,18 @@ from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.linear_model import LogisticRegression
 
-gpu_status = spacy.prefer_gpu()
-nlp = spacy.load('en_core_web_sm', disable=["ner", "parser"])
-lemmatizer = nlp.get_pipe("lemmatizer")
+from imblearn.under_sampling import RandomUnderSampler
+
+stemmer = SnowballStemmer("english")
 punctuation = string.punctuation
-stopwords = set(STOP_WORDS)
+stopwords = set(stopwords.words('english'))
+sampler = RandomUnderSampler(random_state=0)
 
 
-def restrict_labels(labels, threshold=5):
-    defined_labels = []
-    for score in labels:
-        if score < threshold:
-            defined_labels.append(0)
-        else:
-            defined_labels.append(1)
-
-    return defined_labels
-
+def restrict_labels(score, threshold=5):
+    if score < threshold:
+        return 0
+    return 1
 
 def train_model(model, X_train, y_train):
     ''' TODO: train your model based on the training data '''
@@ -40,32 +34,35 @@ def predict(model, X_test):
     labels = model.predict(X_test)
     return labels
 
-
 def tokenize(text):
     tokens = []
     # remove HTML tags and stuff
-    regex = re.compile(r"[A-Za-z0-9\-\'\"?!.,:;()]+") # should we remove punct?
-    cleaned_text = ' '.join(re.findall(regex, text.replace("<br", "")))
-    for token in nlp(cleaned_text):
-        tokens.append(token.lemma_.lower())
+    regex = re.compile(r"[A-Za-z0-9\-]+")
+    cleaned_text = re.findall(regex, text.replace("<br", ""))
+    for token in cleaned_text:
+        tokens.append(stemmer.stem(token).lower())
     
     return tokens
 
 
 if __name__ == "__main__":
-    print("GPU activated?", gpu_status)
     train = pd.read_csv('aclImdb/train_collated.csv')
+    train['Score'] = train['Score'].apply(restrict_labels)
+    train_bal, train_bal['Score'] = sampler.fit_resample(train[['Text']], train['Score']) 
+
     x_train = train['Text']
     y_train = train['Score']
-    y_train = restrict_labels(y_train)
+    x_train_bal = train_bal['Text']
+    y_train_bal = train_bal['Score']
+    print(train_bal.value_counts('Score'))
 
     # model = Pipeline([('vec', CountVectorizer(lowercase=False, tokenizer=spacy_tokenizer)), ('tfidf', TfidfTransformer()), ('mnb', BernoulliNB())])
     model = Pipeline([
-        ('vec', TfidfVectorizer(lowercase=False, tokenizer=tokenize)),
-        ('mnb', BernoulliNB())
+        ('vec', TfidfVectorizer(lowercase=False, tokenizer=tokenize, ngram_range=(1, 2))),
+        ('mnb', LogisticRegression(max_iter=5000))
     ])
 
-    train_model(model, x_train, y_train)
+    train_model(model, x_train_bal, y_train_bal)
     y_pred = predict(model, x_train)
 
     precision = precision_score(y_train, y_pred)
@@ -74,9 +71,9 @@ if __name__ == "__main__":
     print('score on validation for training set: recall=', recall, " precision=", precision, " f1-score=", score)
 
     test = pd.read_csv('aclImdb/test_collated.csv')
+    test['Score'] = test['Score'].apply(restrict_labels)
     x_test = test['Text']
     y_test = test['Score']
-    y_test = restrict_labels(y_test)
     y_pred = predict(model, x_test)
 
     precision = precision_score(y_test, y_pred)
