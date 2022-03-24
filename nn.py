3d653f8902423f3e863ff2194f0b77198ab41019
Aw Chian Hao <e0309371@u.nus.edu.sg>
2022-03-24 18:04:06 +0800

    Neural Network: Single LSTM layer

---
+++ nn.py
@@ -0,0 +1,31 @@
+from keras.metrics import Precision, Recall
+from keras import layers
+from keras import regularizers
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential
+
+def get_metrics(y_true, y_pred):
+	true_pos = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+	#Recall
+	pos = K.sum(K.round(K.clip(y_true, 0, 1)))
+	recall = true_pos / (pos + K.epsilon())
+	#Precision
+	pred_pos = K.sum(K.round(K.clip(y_pred, 0, 1)))
+	precision = true_pos / (pred_pos + K.epsilon())
+	#F1
+	f1 = 2*((precision * recall)/(precision + recall + K.epsilon()))
+	return precision, recall, f1
+
+def run_nn(X_train, y_train, X_test, y_test, max_words, max_len):
+	model = Sequential()
+	model.add(layers.Embedding(max_words, 20, input_length=max_len))
+	model.add(layers.LSTM(15, dropout=0.5))
+	model.add(layers.Dense(2, activation='sigmoid'))
+	model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=[Precision(), Recall()])
+	print(model.summary())
+	checkpoint = ModelCheckpoint("best_model.hdf5", verbose=1, save_best_only=True, mode='auto', save_freq=1, save_weights_only=False)
+	history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+	#y_pred = model.predict(X_test)
+	#precision, recall, f1 = get_metrics(y_test, y_pred)
+	return history
--- nn_preprocessing.py
+++ nn_preprocessing.py
@@ -1,51 +1,97 @@
 import pandas as pd
 import re
-import nltk
+import gensim
+from sklearn.preprocessing import OneHotEncoder
+from imblearn.under_sampling import RandomUnderSampler
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras import regularizers
-from keras import Embedding
+from keras.layers import Embedding
 
-nltk.download('stopwords')
-nltk.download('punkt')
 
+from nn import run_nn
+sampler = RandomUnderSampler(random_state=0)
+ohe = OneHotEncoder()
 
 def clean_text(text):
-    # Convert to lowercase
-    text = text.lower()
-    # Expand contractions
-    text = re.sub(r"'s", r" is", text)
-    text = re.sub(r"'ve", r" have", text)
-    text = re.sub(r"'re'", r"  are", text)
-    text = re.sub(r"can't'", r" cannot", text)
-    text = re.sub(r"n't", r" not", text)
-    text = re.sub(r"'m", r" am", text)
-    # Remove punctuation
-    text = re.sub(r"[^\w \s]", "", text)
-    # Tokenize and remove stopwords
-    tokens = nltk.word_tokenize(text)
-    data = [word for word in tokens if word not in nltk.corpus.stopwords.words("english")]
-    return " ".join(data)
+# Convert to lowercase
+	text = text.lower()
+# Expand contractions
+	#text = re.sub(r"'s", r" is", text)
+	#text = re.sub(r"'ve", r" have", text)
+	#text = re.sub(r"'re'", r"  are", text)
+	#text = re.sub(r"can't'", r" cannot", text)
+	#text = re.sub(r"n't", r" not", text)
+	#text = re.sub(r"'m", r" am", text)
+# Remove punctuation
+	text = re.sub(r"[^\w \s]", "", text)
+	text = re.sub("\'", "", text)
+# Tokenize and remove stopwords
+	#tokens = word_tokenize(text)
+	#data = [word for word in tokens if word not in stopwords.words("english")]
+	return text
+def sent_to_words(sentences):
+	for sentence in sentences:
+		yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))
 
+def detokenize(text):
+	return TreebankWordDetokenizer().detokenize(text)		
 
 def simplify_score(score):
-    score = int(score)
-    if score >= 1 and score <= 5:
-        return -1
-    elif score >= 6 and score <= 10:
-        return 1
-    else:
-        return 0
+	score = int(score)
+	if score > 5:
+		return 1
+	else:
+		return 0
 
 
 if __name__ == '__main__':
-    train = pd.read_csv("./aclImdb/train_collated.csv")
-    X_train = train['Text'].apply(lambda x: clean_text(x))
-    y_train = train['Score'].apply(lambda y : simplify_score(y))
-    max_words = 5000
-    max_len = 200
-    tokenizer = Tokenizer(num_words=max_words)
-    tokenizer.fit_on_texts(X_train)
-    sequences = tokenizer.texts_to_sequences(X_train)
-    reviews = pad_sequences(sequences, maxlen=max_len)
-    embedding_layer = Embedding(1000, 64)
+	train = pd.read_csv("./aclImdb/train_collated.csv")
+	test = pd.read_csv("./aclImdb/test_collated.csv")
+	y_train = train['Score'].apply(lambda y : simplify_score(y))
+	X_train, y_train = sampler.fit_resample(train[['Text']], y_train)
+	y = ohe.fit_transform(y_train.values.reshape(-1, 1)).toarray()
+	y_train = pd.DataFrame(y)
+	print(y_train.head()) 
+	temp = []
+	X_list = X_train.values.tolist()
+	for i in range(len(X_list)):
+		temp.append(clean_text(X_list[i][0]))
+	data_words = list(sent_to_words(temp))	
+	data = []
+	
+	for i in range(len(data_words)):
+		data.append(detokenize(data_words[i]))
+
+	max_words = 5000
+	max_len = 200
+	tokenizer = Tokenizer(num_words=max_words)
+	tokenizer.fit_on_texts(data)
+	sequences = tokenizer.texts_to_sequences(data)
+	reviews = pad_sequences(sequences, maxlen=max_len, padding='post')
+	print(reviews)
+# Repeat for Test set
+	y_test = test['Score'].apply(lambda y : simplify_score(y))
+	yt = ohe.fit_transform(y_test.values.reshape(-1, 1)).toarray()
+	y_test = pd.DataFrame(yt)
+	print(y_test.head())
+	
+	#X_test, y_test = sampler.fit_resample(test[['Text']], y_test)
+	temp = []
+	X_test_list = test[['Text']].values.tolist()
+	for i in range(len(X_test_list)):
+		temp.append(clean_text(X_test_list[i][0]))
+	data_words = list(sent_to_words(temp))
+	test_data = []
+	for i in range(len(data_words)):
+		test_data.append(detokenize(data_words[i]))
+	tokenizer_test = Tokenizer(num_words=max_words)
+	tokenizer_test.fit_on_texts(test_data)
+	test_seq = tokenizer.texts_to_sequences(test_data)
+	test_reviews = pad_sequences(test_seq, maxlen=max_len, padding='post')
+	print(test_reviews)
+	history = run_nn(reviews, y_train, test_reviews, y_test, max_words, max_len)
+	print(history.history)
